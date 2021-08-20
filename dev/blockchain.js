b59e24817ac28803f05e9eab0308dffe8fcf5284
Rcryp <88910817+Ricryp@users.noreply.github.com>
2021-08-20 17:37:36 -0500

    Add files via upload

---
+++ dev/blockchain.js
@@ -0,0 +1,104 @@
+//this file is our blockchain data structure
+
+//dependency requirment 
+const sha256 = require('js-sha256');
+
+//this is a 'constructor' function...data object 
+function Blockchain() {
+    this.chain = []; //initialize the chain to an empty array. We will store all of our blocks here. 
+    this.newTransactions = []; //hold all the new t/x before they are "mined" into a block 
+};
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+    //all of these terms will  soon be revealed
+    const newBlock = {
+        index: this.chain.length + 1,
+        //what block is this in our chain. first, or 1000th? 
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        //all of the t/x in this block
+        nonce: nonce,
+        //a nonce is a number only used once (2, 10, 1232349). this is the PROOF that we actually created a legit. block 
+        hash: hash,
+        //this data from our new block.
+        previousBlockHash: previousBlockHash
+            //data from our current block hashed into a string
+    };
+
+    this.newTransactions = [];
+    //clears out any newTransactions
+    this.chain.push(newBlock);
+    //add the newBlock the the chain 
+
+    return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function() {
+    return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    //create a newTransaction object
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+
+    //add the newTx to the newTx data area
+    this.newTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+    //get the index of the last block of our chain plus one, for a new block. 
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
+    //pascalCase = barryCumbie lower first word, upper each word, no spaces
+    //CamelCase = BarryCumbie
+    //jebob-case = barry-cumbie
+    //snake_case = barri_andrew_cumbie
+    
+    //smoosh all of our 3 parameters into one long string
+    const dataAsString = previousBlockHash + nonce.toString() + currentBlockData + JSON.stringify(currentBlockData);
+    
+    //Pass all of our data as a string into our new, cool js-sha256 library
+    const hash = sha256(dataAsString);
+
+    console.log(dataAsString);
+
+    return hash;
+
+    //What does this method do? Take de blockdata ==> some hash result
+    //for example "cat" ==> "77af778b51abd4a3c51c5ddd97204a9c3ae614ebccb75a606c3b6865aed6744e"
+
+
+
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    //hashing = previus block + curernt block + onnce
+    //set our difficulty level: hash has to start with 0 or 00 or 000 or 0000
+    //brute force, increment nonce & return the hash until we are happy (happy = hash satisfies the business rule/difficulty lvl)
+
+    let nonce = 0; //start are zero, use the let keywork 'cuz this is gonna change
+
+    //create a hash running the hashBlock() method with our default nonce of zero
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+    //execute the code {...} while this condition is true
+    //while the first four digits of the hash are NOT 0000
+    //in java script world 
+    // = mean to assign a value. sha256 = require('js-sha256');
+    // == comparison. does x == y, are they the same, result: true or false
+    // != comparison. not equal. 3 != 5, result: true 
+    // !== 3 !== 3 result: true
+    // === means do they equal and are the same data type
+
+    while (hash.substring(0, 2) != '00'){
+        nonce++
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    return nonce;
+}
+
+module.exports = Blockchain;
